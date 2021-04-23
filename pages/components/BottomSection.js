f802374aa9cff3050241bdb1cd9d2190692790e2
007steve <stephen.plummer1.sp@gmail.com>
2021-04-23 13:03:13 -0400

    added bottom section css

---
+++ pages/components/BottomSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../../styles/BottomSection.module.css";
+function BottomSection() {
+    return (
+        <div className={styles.bottomSection}>
+            
+        </div>
+    )
+}
+
+export default BottomSection
